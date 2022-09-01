7a759090eccc540e6dc255393c68f72125c07434
Zablotskaya Dasha <dasaz659@gmail.com>
2022-09-01 21:24:50 +0300

    Problem2

---
+++ lesson5/tasks/tasks/Problem2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tasks
+{
+    static class Problem2
+    {
+        public static void Solve()
+        {
+            int n = Number();
+            AbsoluteDifference(n);
+        }
+        static int Number()
+        {
+            Console.WriteLine("Enter number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            return number;
+        }
+        static void AbsoluteDifference(int n)
+        {
+            if (n>51)
+            {
+                Console.WriteLine($"rez=3*({n}-51)={3 * (n - 51)}");
+            }
+            else
+            {
+                Console.WriteLine($"rez=51-{n}={51 - n}");
+            }
+        }
+    }
+}
--- lesson5/tasks/tasks/Program.cs
+++ lesson5/tasks/tasks/Program.cs
@@ -6,7 +6,7 @@ namespace tasks
     {
         static void Main(string[] args)
         {
-            Problem1.Solve();
+            Problem2.Solve();
         }
     }
 }
