360ad88f3f4d40128f1385393fa42e706a1e713d
Zablotskaya Dasha <dasaz659@gmail.com>
2022-09-01 20:12:00 +0300

    create metods of operation

--- homework4/calculator/calculator/Program.cs
+++ homework4/calculator/calculator/Program.cs
@@ -20,60 +20,108 @@ namespace calculator
             }
 
         }
+
+        static void Plus(int a, int b)
+        {
+            Console.WriteLine($"c={a}+{b}={a+b}");
+        }
+
+        static void Minus(int a, int b)
+        {
+            Console.WriteLine($"c={a}-{b}={a - b}");
+        }
+        static void Division(int a, int b)
+        {
+            Console.WriteLine($"c={a}/{b}={a / b}");
+        }
+
+        static void Multiplication (int a, int b)
+        {
+            Console.WriteLine($"c={a}*{b}={a * b}");
+        }
+
+        static void Percent(int a, int b)
+        {
+            Console.WriteLine($"c={a}%{b}={a % b}");
+        }
+        static void Power(int a, int b)
+        {
+            Console.WriteLine("To what degree do you want to raise the numbers?");
+            int stepen = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose one number: a or b");
+            string sravn = Console.ReadLine();
+            if (sravn == "a")
+            {
+                Console.WriteLine($"c={a}^{stepen}={Math.Pow(a, stepen)}");
+            }
+            else if (sravn == "b")
+            {
+                Console.WriteLine($"c={b}^{stepen}={Math.Pow(b, stepen)}");
+            }
+        }
+        static void Square(int a, int b)
+        {
+            Console.WriteLine("Choose one number: a or b");
+            string sravne = Console.ReadLine();
+            if (sravne == "a")
+            {
+                Console.WriteLine($"c={Math.Sqrt(a)}");
+            }
+            else if (sravne == "b")
+            {
+                Console.WriteLine($"c={Math.Sqrt(b)}");
+            }
+        }
+        static void SpecialForDefault()
+        {
+            Console.WriteLine("Error! There is no such operation in the calculator!");
+        }
+       
+        static int Param()
+        {
+            Console.WriteLine("Enter two numbers");
+            int peremen = Convert.ToInt32(Console.ReadLine());
+            return peremen;
+
+        }
         static void Operation()
         {
+
             Console.WriteLine("Which operation do you want to use?");
             string nameOperation = Console.ReadLine();
-            Console.WriteLine("Enter two numbers");
+            int a = Param();
+            int b = Param();
+
+
+           /* Console.WriteLine("Enter two numbers");
             int a = Convert.ToInt32(Console.ReadLine());
             int b = Convert.ToInt32(Console.ReadLine());
+           */
             switch (nameOperation)
             {
                 case "+":
-                    Console.WriteLine($"c={a}+{b}={a + b}");
+                    Plus(a, b);
                     break;
                 case "-":
-                    Console.WriteLine($"c={a}-{b}={a - b}");
+                    Minus(a, b);
                     break;
                 case "/":
-                    Console.WriteLine($"c={a}/{b}={a / b}");
+                    Division(a, b);
                     break;
                 case "*":
-                    Console.WriteLine($"c={a}*{b}={a * b}");
+                    Multiplication(a, b);
                     break;
-
-                    // % - остаток от деления
                 case "%":
-                    Console.WriteLine($"c={a}%{b}={a % b}");
+                    Percent(a, b);
                     break;
                 case "pow":
-                    Console.WriteLine("To what degree do you want to raise the numbers?");
-                    int d = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Choose one number: a or b");
-                    string h = Console.ReadLine();
-                    if (h == "a")
-                    {
-                        Console.WriteLine($"c={a}^{d}={Math.Pow(a, d)}");
-                    }
-                    else if (h == "b")
-                    {
-                        Console.WriteLine($"c={b}^{d}={Math.Pow(b, d)}");
-                    }
+                    Power(a, b);
                     break;
                 case "sqrt":
-                    Console.WriteLine("Choose one number: a or b");
-                    string j = Console.ReadLine();
-                    if (j == "a")
-                    {
-                        Console.WriteLine($"c={Math.Sqrt(a)}");
-                    }
-                    else if (j == "b")
-                    {
-                        Console.WriteLine($"c={Math.Sqrt(b)}");
-                    }
+                    Square(a, b);
                     break;
                 default:
-                    Console.WriteLine("Error! There is no such operation in the calculator!");
+                    SpecialForDefault();
                     break;
             }
         }
