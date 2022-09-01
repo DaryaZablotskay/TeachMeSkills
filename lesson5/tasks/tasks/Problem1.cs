a322d1e5bf6695606340bfc7ef4ec927e809da70
Zablotskaya Dasha <dasaz659@gmail.com>
2022-09-01 21:15:41 +0300

    Problem1

---
+++ lesson5/tasks/tasks/Problem1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tasks
+{
+    static class Problem1
+    {
+        public static void Solve()
+        {
+            int a = Peremen();
+            int b = Peremen();
+            Sum(a, b);
+
+        }
+        static int Peremen()
+        {
+            Console.WriteLine("Enter number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            return number;
+        }
+        static void Sum(int a, int b)
+        {
+            if(a==b)
+            {
+                Console.WriteLine($"sum=3*({a}+{b})={3*(a + b)}");
+            }
+            else
+            {
+                Console.WriteLine($"sum={a}+{b}={a + b}");
+            }
+            
+        }
+    }
+}
