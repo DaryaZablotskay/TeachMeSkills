b632dbaba144bb6996ea512a0ab5eb0dd62af056
Zablotskaya Dasha <dasaz659@gmail.com>
2022-09-10 18:44:56 +0300

    calculator

--- homework4/calculator/calculator/Program.cs
+++ homework4/calculator/calculator/Program.cs
@@ -6,12 +6,11 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-            string answer = DialogWithUser();
-
+            
             do
             {
                 string nameOperation = NameOfOperation();
-                if (nameOperation=="8")
+                if (nameOperation=="Full")
                 {
                     EnterExpression();
                 }
@@ -19,86 +18,253 @@ namespace calculator
                 {
                     try
                     {
-                        int a = Param();
-                        int b = Param();
+                        double a = Param();
+                        double b = Param();
                         FinalRezult(nameOperation, a, b);
+                     
                     }
-                    catch(Exception e)
+                    catch (FormatException)
                     {
-                        Console.WriteLine("oops");
+                        Console.WriteLine("Oops! Enter the correct number!");
+                        double c = Param();
+                        double d = Param();
+                        FinalRezult(nameOperation, c, d);
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Oops! Enter the second number not equal to zero!");
+                        double c = Param();
+                        double d = Param();
+                        FinalRezult(nameOperation, c, d);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Oops! Something went wrong!");
+                        double c = Param();
+                        double d = Param();
+                        FinalRezult(nameOperation, c, d);
                     }
                 }
-                
-                
-                Console.WriteLine("Do you want to continue?");
-                answer = Console.ReadLine();
-            } while (answer == "yes");
+            } while (DialogWithUser() == "yes");
         }
+        static void FinalRezult(string nameOperation, double a, double b)
+        {
+            
+                switch (nameOperation)
+                {
+                    case "+":
+                        Plus(a, b);
+                        break;
+                    case "-":
+                        Minus(a, b);
+                        break;
+                    case "/":
+                        Division(a, b);
+                        break;
+                    case "*":
+                        Multiplication(a, b);
+                        break;
+                    case "%":
+                        Percent(a, b);
+                        break;
+                    case "^":
+                        Power(a, b);
+                        break;
+                    case "sqrt":
+                        Square(a, b);
+                        break;
+                    case "Full":
+                        EnterExpression();
+                        break;
+                    default:
+                        SpecialForDefault();
+                        break;
+                
+            }
 
+        }
         static string DialogWithUser()
         {
             Console.WriteLine("Do you want to continue?");
             string answer = Console.ReadLine();
             return answer;
         }
-       
-        static void Plus(int a, int b)
-        {
-            Console.WriteLine($"c={a}+{b}={a+b}");
-        }
 
-        static void Minus(int a, int b)
+        static void Plus(double a, double b)
         {
-            Console.WriteLine($"c={a}-{b}={a - b}");
-        }
-        static void Division(int a, int b)
-        {
-            Console.WriteLine($"c={a}/{b}={a / b}");
-        }
+            
+            double c = a + b;
+            int count1 = NumberAfterPoint(a);
+            int count2 = NumberAfterPoint(b);
+            if(count2>count1)
+            {
+               double rez = Math.Round(c, count2);
+                Console.WriteLine($"c={a}+{b}={rez}");
+            }
+            else
+            {
+                double rez = Math.Round(c, count1);
+                Console.WriteLine($"c={a}+{b}={rez}");
+            }
 
-        static void Multiplication (int a, int b)
-        {
-            Console.WriteLine($"c={a}*{b}={a * b}");
         }
-
-        static void Percent(int a, int b)
+        static void Minus(double a, double b)
         {
-            Console.WriteLine($"c={a}%{b}={a % b}");
+            double c = a - b;
+            int count1 = NumberAfterPoint(a);
+            int count2 = NumberAfterPoint(b);
+            if (count2 > count1)
+            {
+                double rez = Math.Round(c, count2);
+                Console.WriteLine($"c={a}-{b}={rez}");
+            }
+            else
+            {
+                double rez = Math.Round(c, count1);
+                Console.WriteLine($"c={a}-{b}={rez}");
+            }
         }
-        static void Power(int a, int b)
+        static void Division(double a, double b)
+        {
+            double c = a / b;
+            int count1 = NumberAfterPoint(a);
+            int count2 = NumberAfterPoint(b);
+            if (count2 > count1)
+            {
+                double rez = Math.Round(c, count2);
+                Console.WriteLine($"c={a}/{b}={rez}");
+            }
+            else
+            {
+                double rez = Math.Round(c, count1);
+                Console.WriteLine($"c={a}/{b}={rez}");
+            }
+        }
+        static void Multiplication (double a, double b)
+        {
+            double c = a * b;
+            int count1 = NumberAfterPoint(a);
+            int count2 = NumberAfterPoint(b);
+            if (count2 > count1)
+            {
+                double rez = Math.Round(c, count2);
+                Console.WriteLine($"c={a}*{b}={rez}");
+            }
+            else
+            {
+                double rez = Math.Round(c, count1);
+                Console.WriteLine($"c={a}*{b}={rez}");
+            }
+        }
+        static void Percent(double a, double b)
+        {
+            double c = a % b;
+            int count1 = NumberAfterPoint(a);
+            int count2 = NumberAfterPoint(b);
+            if (count2 > count1)
+            {
+                double rez = Math.Round(c, count2);
+                Console.WriteLine($"c={a}%{b}={rez}");
+            }
+            else
+            {
+                double rez = Math.Round(c, count1);
+                Console.WriteLine($"c={a}%{b}={rez}");
+            }
+        }
+        static void Power(double a, double b)
         {
             Console.WriteLine("To what degree do you want to raise the numbers?");
-            int stepen = Convert.ToInt32(Console.ReadLine());
+            double stepen = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Choose one number: a or b");
             string sravn = Console.ReadLine();
-            if (sravn == "a")
+            
+            int count1 = NumberAfterPoint(a);
+            int count2 = NumberAfterPoint(b);
+            if (count2 > count1)
             {
-                Console.WriteLine($"c={a}^{stepen}={Math.Pow(a, stepen)}");
+                if (sravn == "a")
+                {
+                    double rez = Math.Round(Math.Pow(a,stepen), count2);
+                    Console.WriteLine($"c={a}^{stepen}={rez}");
+                }
+                else if (sravn == "b")
+                {
+                    double rez = Math.Round(Math.Pow(b, stepen), count2);
+                    Console.WriteLine($"c={b}^{stepen}={rez}");
+                }
             }
-            else if (sravn == "b")
+            else
             {
-                Console.WriteLine($"c={b}^{stepen}={Math.Pow(b, stepen)}");
-            }
+                if (sravn == "a")
+                {
+                    double rez = Math.Round(Math.Pow(a, stepen), count1);
+                    Console.WriteLine($"c={a}^{stepen}={rez}");
+                }
+                else if (sravn == "b")
+                {
+                    double rez = Math.Round(Math.Pow(b, stepen), count1);
+                    Console.WriteLine($"c={b}^{stepen}={rez}");
+                }
+            } 
         }
-        static void Square(int a, int b)
+        static void Square(double a, double b)
         {
             Console.WriteLine("Choose one number: a or b");
             string sravne = Console.ReadLine();
-            if (sravne == "a")
+            int count1 = NumberAfterPoint(a);
+            int count2 = NumberAfterPoint(b);
+            if (count2 > count1)
             {
-                Console.WriteLine($"c={Math.Sqrt(a)}");
+                if (sravne == "a")
+                {
+                    double rez = Math.Round(Math.Sqrt(a), count2);
+                    Console.WriteLine($"c={rez}");
+                }
+                else if (sravne == "b")
+                {
+                    double rez = Math.Round(Math.Sqrt(b), count2);
+                    Console.WriteLine($"c={rez}");
+                }
             }
-            else if (sravne == "b")
+            else
             {
-                Console.WriteLine($"c={Math.Sqrt(b)}");
-            }
+                if (sravne == "a")
+                {
+                    double rez = Math.Round(Math.Sqrt(a), count1);
+                    Console.WriteLine($"c={rez}");
+                }
+                else if (sravne == "b")
+                {
+                    double rez = Math.Round(Math.Sqrt(b), count1);
+                    Console.WriteLine($"c={rez}");
+                }
+            }   
         }
         static void EnterExpression()
         {
             string[] oper = { "+", "-", "/", "*", "%", "^", "sqrt" };
             string a = Expression();
-            
+            string[] b = a.Split('+', '-', '/', '*', '%', '^') ;
+            for(int i=0; i<oper.Length; i++)
+            {
+                bool c = a.Contains(oper[i]);
+                if (c)
+                {
+                    for(int j=0; j<b.Length; j++)
+                    {
+                        /*
+                        double b1 = Convert.ToDouble(b[j]);
+                        double b2 = Convert.ToDouble(b[++j]);
+                        Console.WriteLine($"{b1} {b2}");
+                        */
+                        string nameOperation = oper[i];
+                        FinalRezult(nameOperation, Convert.ToDouble(b[j]), Convert.ToDouble(b[++j]));
+                    }
+                }
 
+            }
+            /*
             for(int i=0; i<oper.Length; i++)
             {
                bool c = a.Contains(oper[i]);
@@ -107,20 +273,19 @@ namespace calculator
                     string[] b = a.Split(oper[i]);
                     try
                     {
-                        int b1 = Convert.ToInt32(b[0]);
-                        int b2 = Convert.ToInt32(b[1]);
+                        double b1 = Convert.ToDouble(b[0]);
+                        double b2 = Convert.ToDouble(b[1]);
                         string nameOperation = oper[i];
                         FinalRezult(nameOperation, b1, b2);
                     }
-                    catch(Exception e)
+                    catch (Exception)
                     {
-                        Console.WriteLine("oops!");
+                        Console.WriteLine("Oops! Something went wrong!");
+                        EnterExpression();
                     }
-                    
                 }
-
             }
-
+            */
 
         }
         static void SpecialForDefault()
@@ -128,12 +293,19 @@ namespace calculator
             Console.WriteLine("Error! There is no such operation in the calculator!");
         }
        
-        static int Param()
+        static string NameOfOperation()
         {
-            Console.WriteLine("Enter number");
-            int peremen = Convert.ToInt32(Console.ReadLine());
-            return peremen;
-
+            Console.WriteLine("List of operations:");
+            Console.WriteLine($"+ {"\n"}- {"\n"}/ {"\n"}* {"\n"}% {"\n"}^ {"\n"}sqrt {"\n"}Full {"\n"}");
+            Console.WriteLine("Which operation do you want to use?");
+            string nameOfOperation = Console.ReadLine();
+            return nameOfOperation;
+        }
+        static double Param()
+        {
+             Console.WriteLine("Enter number");
+             double peremen = Convert.ToDouble(Console.ReadLine());
+             return peremen;
         }
         static string Expression()
         {
@@ -141,65 +313,17 @@ namespace calculator
             string expres = Console.ReadLine();
             return expres;
         }
-        static string NameOfOperation()
+        static int NumberAfterPoint(double a)
         {
-            Console.WriteLine("1.+");
-            Console.WriteLine("2.-");
-            Console.WriteLine("3./");
-            Console.WriteLine("4.*");
-            Console.WriteLine("5.%");
-            Console.WriteLine("6.^");
-            Console.WriteLine("7.sqrt");
-            Console.WriteLine("8.Enter expression");
-            Console.WriteLine("Which operation do you want to use?");
-            
-            string nameOfOperation = Console.ReadLine();
-            return nameOfOperation;
-           
-        }
-
-        
-           
-        
-        static void FinalRezult(string nameOperation, int a, int b)
-        {
-
-            
-
-            switch (nameOperation)
-                {
-                case "+":
-                        Plus(a, b);
-                        break;
-                case "-":
-                        Minus(a, b);
-                        break;
-                case "/":
-                        Division(a, b);
-                        break;
-                case "*":
-                        Multiplication(a, b);
-                        break;
-                case "%":
-                        Percent(a, b);
-                        break;
-                case "^":
-                        Power(a, b);
-                        break;
-                case "sqrt":
-                        Square(a, b);
-                        break;
-                case "8":
-                    EnterExpression();
-                    break;
-
-
-                default:
-                        SpecialForDefault();
-                        break;
-               
+            int i = 1,
+                count = 0;
+            while((a*Math.Pow(10,i))%10 != 0)
+            {
+                count++;
+                i++;
             }
-            
+            return count;
         }
+
     }
 }
