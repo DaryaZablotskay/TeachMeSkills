71de7ae2a15507f681580b0236fc0a7cb7d1476c
Zablotskaya Dasha <dasaz659@gmail.com>
2022-10-11 19:56:54 +0300

    exception

---
+++ lesson12/lesson12/lesson12/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesson12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InputRoutin();
+            }
+            catch(MyException e) when (string.IsNullOrWhiteSpace(e.Message))
+            {
+                Console.WriteLine("Sorry");
+            }
+            catch (MyException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        public static void InputRoutin()
+        {
+            Console.WriteLine("Input number");
+            var input = Console.ReadLine();
+            
+            try
+            {
+                var i = ProcessInput(input);
+            }
+            catch (MyException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+
+                //throw;
+                //throw e;
+            }
+            finally
+            {
+
+            }
+        }
+        public static int ProcessInput(string input)
+        {
+            //var isNumber = int.TryParse(input, out var i);
+            var isNumber = int.TryParse(input ?? throw new ArgumentNullException(), out var i);
+            return isNumber == true? i: throw new MyException("Input must be number");
+        }
+    }
+
+    public class SomeClass
+    {
+        private string _stroka;
+        public SomeClass(string stroka)
+        {
+            _stroka = stroka ?? throw new ArgumentNullException(nameof(stroka));
+        }
+    }
+}
