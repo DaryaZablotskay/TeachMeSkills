7871a402c6ff0ec6eece812e5caa015895600d00
Zablotskaya Dasha <dasaz659@gmail.com>
2022-10-17 22:41:53 +0300

    start work with file

--- homework11/homework11/homework11/Program.cs
+++ homework11/homework11/homework11/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 
 namespace homework11
 {
@@ -6,7 +8,73 @@ namespace homework11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var caffe1 = new Caffe()
+            { 
+                Name="Cat",
+                Emploee=10,
+                Information =new Information
+                {
+                    Street="Pushkina",
+                    City="Minsk",
+                    PhoneNumber=1234566
+                }
+            };
+            var caffe2 = new Caffe()
+            {
+                Name = "Dog",
+                Emploee = 12,
+                Information = new Information
+                {
+                    Street = "MinskMir",
+                    City = "Minsk",
+                    PhoneNumber = 9864389
+                }
+            };
+            var caffe3 = new Caffe()
+            {
+                Name = "Pet",
+                Emploee = 7,
+                Information = new Information
+                {
+                    Street = "",
+                    City = "Minsk",
+                    PhoneNumber = 3820790
+                }
+            };
+            FileStream stream = File.Open("caffe.json", FileMode.OpenOrCreate);
+            JsonSerializer js = new JsonSerializer();
+            var sw = new StreamWriter(stream);
+            js.Serialize(sw, caffe1);
+            sw.Flush();
+
+            stream.Seek(0, SeekOrigin.Begin);
+
+            //var streamReader = new StreamReader(stream);
+            //var deserializedObj = (Caffe)js.Deserialize(streamReader, typeof(Caffe));
+            //Console.WriteLine(deserializedObj);
+
+            stream.Close();
         }
     }
+    public class Caffe
+    {
+        public string Name { get; set; }
+        public int Emploee { get; set; }
+        public Information Information { get; set; }
+        public Caffe()
+        {
+        }
+
+    }
+    public class Information
+    { 
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int PhoneNumber { get; set; }
+        public override string ToString()
+        {
+            return $"{City} {Street} {PhoneNumber}";
+        }
+    }
+
 }
