d621b6c9324f84790db204e3a6256a0d31383b7c
Zablotskaya Dasha <dasaz659@gmail.com>
2022-10-20 00:55:23 +0300

    queue

---
+++ Collection/Queue/Queue/MyQueue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queue
+{
+    public class MyQueue<T>
+    {
+        private List<T> items = new List<T>();
+        public MyQueue()
+        {
+        }
+        public void Enqueue(T item)
+        {
+            items.Add(item);
+        }
+        public T Dequeue()
+        {
+            if(items.Count>0)
+            {
+                T item = items[0];
+                items.Remove(item);
+                return item;
+            }
+            else
+            {
+                throw new Exception("queue null");
+            }
+        }
+        public void Print()
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                Console.WriteLine(items[i]);
+            }
+        }
+    }
+}
---
+++ Collection/Queue/Queue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new MyQueue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+
+            queue.Dequeue();
+
+            queue.Print();
+        }
+    }
+}
