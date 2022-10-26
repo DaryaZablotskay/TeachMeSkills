8569129d48b2e0a3d0ed62d10ceb9849f579fa1c
Zablotskaya Dasha <dasaz659@gmail.com>
2022-10-27 00:44:11 +0300

    task 1 and 2

---
+++ HW_25.10/HW_25.10/Controllers/NewController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW_25._10.Controllers
+{
+    [ApiController]
+    [Route("task")]
+    public class NewController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public NewController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [HttpGet("first")]
+        public string GetFirst()
+        {
+            return _config.GetValue<string>("DataFile");
+        }
+        [HttpGet("second")]
+        public string GetSecond()
+        {
+            var path = _config.GetValue<string>("DataFile1");
+            string text = null;
+            using(StreamReader streamReader = new StreamReader(path))
+            {
+                text = streamReader.ReadToEnd();
+            }
+            return text;
+        }
+    }
+}
---
+++ HW_25.10/HW_25.10/Startup.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW_25._10
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddOpenApiDocument(config =>
+            {
+                config.Title = "HoMeWoRk";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseOpenApi(); // serve OpenAPI/Swagger documents
+            app.UseSwaggerUi3(config =>
+            {
+                config.DocumentTitle = "My Web Site";
+                config.DocExpansion = "list";
+            }
+                ); // serve Swagger UI
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
